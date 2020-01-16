0db2e72e4aaf026195488434bf3e846adbad108c
Mamathap37 <mamatha@bytesqueue.com>
2020-01-16 12:09:46 +0530

    first commit

---
+++ app/controllers/robots_controller.rb
@@ -0,0 +1,40 @@
+class RobotsController < ApplicationController
+  def index
+  @robots = Robot.all
+  end
+  
+  def show
+  @robot = Robot.find(params[:id])
+  end
+  
+  def new
+    @robot = Robot.new
+  end
+
+  def create
+    @robot = Robot.create(robot_params)
+
+    redirect_to robots_path(@robot)
+  end
+  
+  def edit
+  	@robot = Robot.find(params[:id])
+  end
+
+   def update
+    @robot = Robot.find(params[:id])
+    @robot.update robot_params
+  end
+  
+  def destroy
+    @robot = Robot.find(params[:id])
+    @robot.destroy
+
+    redirect_to robots_path
+  end
+
+  private
+  def robot_params
+    params.require(:robot).permit(:name, :slogan)
+  end
+end
